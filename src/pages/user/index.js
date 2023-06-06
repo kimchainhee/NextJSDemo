4ef31157c3a7068560c5cc42fe4fc9849ef433b0
Dev Phi <kusphan15@gmail.com>
2023-06-06 13:37:45 +0700

    [Update] Source layout next js 09 06/06

--- src/pages/user/index.js
+++ src/pages/user/index.js
@@ -1,24 +1,36 @@
-// 'use client'
+'use client'
 import { Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper, Typography, Grid, TextField, Button, Box } from "@mui/material";
 import Link from "next/link";
 import React, { useState, useEffect } from "react"
 
-function createData(id, name, email, phone) {
-  return { id, name, email, phone };
+const createArray = (max = 10) => {
+  const arr = [];
+  for (let index = 0; index < max; index += 1) {
+    arr[index] = index;
+  }
+  return arr;
+};
+
+function randomNumber(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-const rows = [
-  { name: 'Anh1', email: 'anh1@gmail.com', phone: '321231321' },
-  { name: 'Anh2', email: 'anh2@gmail.com', phone: '654564654' },
-  { name: 'Anh3', email: 'anh3@gmail.com', phone: '987987987' },
-];
+const getRows = (length = 10) =>
+  createArray(length).map(item => {
+
+    return {
+      name: `Anh${item + 1}`,
+      email: `anh${item + 1}@gmail.com`,
+      phone: randomNumber(1000000000, 9999999999),
+    };
+  });
 
 
 export default function ListUser() {
   const [data, setData] = useState([]);
 
   useEffect(() => {
-    setData(rows);
+    setData(getRows());
   }, []);
 
   const [inputName, setInputName] = useState('');
@@ -26,10 +38,29 @@ export default function ListUser() {
   const [inputPhone, setInputPhone] = useState('');
 
   const handleAdd = () => {
-    const newData = [...data, { name: inputName, email: inputEmail, phone: inputPhone }];
     if (inputName && inputEmail && inputPhone !== '') {
+      setInputName('');
+      setInputEmail('');
+      setInputPhone('');
+      const newData = [...data, { name: inputName, email: inputEmail, phone: inputPhone }];
       return setData(newData);
     }
+  };
+
+  const handleClear = () => {
+    setInputName('');
+    setInputEmail('');
+    setInputPhone('');
+  };
+
+  const handleDelete = index => {
+    const arrResult = [...data];
+
+    if (index > -1) {
+      arrResult.splice(index, 1);
+    }
+
+    setData(arrResult);
   };
 
   return (
@@ -38,23 +69,40 @@ export default function ListUser() {
         <b>ListUser</b>
       </Typography>
       <Grid container spacing={2}>
-        <Grid item xs={3}>
-          <TextField label='Name' fullWidth size="small" onChange={e => setInputName(e.target.value)} />
+        <Grid item xs={12} md={3}>
+          <TextField
+            label='Name'
+            focused
+            fullWidth size="small"
+            value={inputName}
+            onChange={e => setInputName(e.target.value)}
+          />
         </Grid>
-        <Grid item xs={3}>
-          <TextField label='Email' fullWidth size="small" onChange={e => setInputEmail(e.target.value)} />
+        <Grid item xs={12} md={3}>
+          <TextField
+            label='Email'
+            focused
+            fullWidth size="small"
+            value={inputEmail}
+            onChange={e => setInputEmail(e.target.value)} />
         </Grid>
-        <Grid item xs={3}>
-          <TextField label='Phone' fullWidth type='number' size="small" onChange={e => setInputPhone(e.target.value)} />
+        <Grid item xs={12} md={3}>
+          <TextField
+            label='Phone'
+            focused
+            fullWidth type='number'
+            value={inputPhone} size="small"
+            onChange={e => setInputPhone(e.target.value)} />
         </Grid>
-        <Grid item xs>
+        <Grid item xs={12} md={3}>
           <Box align='right'>
+            <Button variant='outlined' onClick={handleClear}>Clear</Button>{' '}
             <Button variant='contained' onClick={handleAdd}>Add</Button>
           </Box>
         </Grid>
         <Grid item xs={12}>
           <TableContainer component={Paper} variant='outlined'>
-            <Table sx={{ minWidth: 650 }} aria-label="simple table">
+            <Table sx={{ minWidth: 650 }} aria-label="simple table" size="small">
               <TableHead>
                 <TableRow>
                   <TableCell><b>Id</b></TableCell>
@@ -67,20 +115,20 @@ export default function ListUser() {
               <TableBody>
                 {data.map((row, index) => {
                   const key = index;
-                  const dataRender = createData(index + 1, row.name, row.email, row.phone)
                   return (
                     <TableRow
                       key={key}
                       sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
                     >
                       <TableCell component="th" scope="row">
-                        {dataRender.id}
+                        {index + 1}
                       </TableCell>
                       <TableCell>
                         <Typography
+                          variant='body2'
                           color='primary'
                           component={Link}
-                          href={`/user/id:${dataRender.id}`}
+                          href={`/user/id:${row.id}`}
                           sx={{
                             cursor: 'pointer',
                             textDecoration: 'none',
@@ -89,13 +137,18 @@ export default function ListUser() {
                             }
                           }}
                         >
-                          {dataRender.name}
+                          {row.name}
                         </Typography>
                       </TableCell>
-                      <TableCell>{dataRender.email}</TableCell>
-                      <TableCell align="right">{dataRender.phone}</TableCell>
+                      <TableCell>{row.email}</TableCell>
+                      <TableCell align="right">{row.phone}</TableCell>
                       <TableCell align="right">
-                        <Button variant='outlined' color='error'>
+                        <Button
+                          variant='outlined'
+                          color='error'
+                          size="small"
+                          onClick={() => handleDelete(index)}
+                        >
                           Delete
                         </Button>
                       </TableCell>
